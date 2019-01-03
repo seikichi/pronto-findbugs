0a0b8e2848bb7e177c840c9b858f8536f94adc35
Seiichi KONDO <seikichi@kmc.gr.jp>
2019-01-03 23:07:41 +0900

    Use test-unit instead of minitest

--- test/pronto/findbugs_test.rb
+++ test/pronto/findbugs_test.rb
@@ -1,13 +1,7 @@
 require 'test_helper'
 
-module Pronto
-  class FindbugsTest < Minitest::Test
-    def test_that_it_has_a_version_number
-      refute_nil ::Pronto::Findbugs::VERSION
-    end
-
-    def test_it_does_something_useful
-      assert false
-    end
+class FindbugsTest < Test::Unit::TestCase
+  def test_that_it_has_a_version_number
+    refute_nil ::Pronto::Findbugs::VERSION
   end
 end
